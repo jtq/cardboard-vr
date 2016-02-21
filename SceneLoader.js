e720101c17c1575df62d0d7033f3c163a4b3dc83
jtq <jamestpalmer1979@gmail.com>
2016-02-21 22:19:02 +0000

    Clean-up and moved object creation logic into SceneLoader

--- SceneLoader.js
+++ SceneLoader.js
@@ -1,4 +1,5 @@
 var sceneLoader = {
+
 	load: function(url, callback) {
 		var request = new XMLHttpRequest();
 		request.onreadystatechange = function () {
@@ -9,5 +10,22 @@ var sceneLoader = {
 		};
 		request.open('GET', url, true);
 		request.send();
+	},
+
+	inflate: function(objData) {
+
+		var meshes = [];
+
+		objData.forEach(function(obj) {
+			var geometry = new THREE[obj.geo](obj.dim[0],obj.dim[1],obj.dim[2]);
+			var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color:Number(obj.col) }));
+			mesh.position.set(obj.pos[0], obj.pos[1]-10, obj.pos[2]);
+			mesh.rotation.set(deg2rad(obj.rot[0]), deg2rad(obj.rot[1]), deg2rad(obj.rot[2]));
+			mesh.name = obj.name;
+			meshes.push(mesh);
+		});
+
+		return meshes;
 	}
+
 };
