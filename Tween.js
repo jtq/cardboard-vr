909cd66f41cd728a8bd24cf8938c43c20b5f1594
jtq <jamestpalmer1979@gmail.com>
2016-03-12 23:15:43 +0000

    Added "delay" animation field
    Also fixed bug in Tweening library that captured "original value" too
    early (on animation start, rather than process-delay-expired).  This
    meant that out-and-in-again animations would have their second halves
    fail, because it was trying to tween from (animation-start) "in" to "in"
    values again, instead of the "out" value the previous animation process
    left it at.

--- Tween.js
+++ Tween.js
@@ -12,16 +12,7 @@ var tween = {
 			processes: JSON.parse(JSON.stringify(processes)),	// Hacky shortcut to copy simple object
 		};
 
-		animation.processes.forEach(function(process) {
-			process.origValue = {};
-
-			Object.keys(process.val).forEach(function(key) {
-				process.origValue[key] = object[process.prop][key];
-			});
-		});
-		
 		this.animations.push(animation);
-		//console.log(animation);
 	},
 
 	render: function() {
@@ -37,39 +28,58 @@ var tween = {
 
 			anim.processes.forEach(function(process, processIndex) {
 
-				var fraction = (currentTime - anim.start) / process.time;
+				process.delay = process.delay || 0;
 
-				Object.keys(process.val).forEach(function(key) {
-					var originalValue = process.origValue[key];
-					var targetValue = process.val[key];
-					var currentValue = ((targetValue - originalValue) * fraction) + originalValue;
+				var timeSinceStart = currentTime - anim.start;
 
-					//console.log(key, originalValue + '->' + targetValue, self.progress, fraction, '=', currentValue);
+				if(timeSinceStart > process.delay) {
+
+					// If this is the first step of this animation, capture the original value at this point (before we modify it, but after any earlier processes have modified it)
+					if(!process.origValue) {
+						process.origValue = {};
+
+						Object.keys(process.val).forEach(function(key) {
+							process.origValue[key] = anim.object[process.prop][key];
+						});
+					}
+
+
+					var timeintoAnim = timeSinceStart - process.delay;
+
+					var fraction = timeintoAnim / process.time;
+
+					Object.keys(process.val).forEach(function(key) {
+						var originalValue = process.origValue[key];
+						var targetValue = process.val[key];
+						var currentValue = ((targetValue - originalValue) * fraction) + originalValue;
+
+						//console.log(key, originalValue + '->' + targetValue, self.progress, fraction, '=', currentValue);
+
+						if(fraction < 1) {	// If process is ongoing, set to new value
+							anim.object[process.prop][key] = currentValue;
+						}
+						else {	// If process has ended, explicitly set to target value (to avoid rounding errors)
+							anim.object[process.prop][key] = process.val[key];
+						}
+					});
+
+					// Now check whether the process, animation or all animations have finished, and if so drop them from consideration
 
 					if(fraction < 1) {	// If process is ongoing, set to new value
-						anim.object[process.prop][key] = currentValue;
+						//console.log("incremented process", (fraction*100).toFixed(2)+"%", anim.object.name, process.prop, process.val);
 					}
-					else {	// If process has ended, explicitly set to target value (to avoid rounding errors)
-						anim.object[process.prop][key] = process.val[key];
-					}
-				});
+					else {	// If process has ended, drop the process from the animation
+						anim.processes.splice(processIndex, 1);
+						//console.log("removed process", anim.object.name, process.prop, process.val, anim.processes);
 
-				// Now check whether the process, animation or all animations have finished, and if so drop them from consideration
+						if(!anim.processes.length) {	// If no more processes in the animation, drop the animation altogether
+							self.animations.splice(animIndex,1);
+							//console.log("removed anim", anim.object.name, self.animations);
 
-				if(fraction < 1) {	// If process is ongoing, set to new value
-					//console.log("incremented process", (fraction*100).toFixed(2)+"%", anim.object.name, process.prop, process.val);
-				}
-				else {	// If process has ended, drop the process from the animation
-					anim.processes.splice(processIndex, 1);
-					//console.log("removed process", anim.object.name, process.prop, process.val, anim.processes);
-
-					if(!anim.processes.length) {	// If no more processes in the animation, drop the animation altogether
-						self.animations.splice(animIndex,1);
-						//console.log("removed anim", anim.object.name, self.animations);
-
-						if(!self.animations.length) {
-							//console.log('finished all animations - auto-stopping');
-							self.stop();
+							if(!self.animations.length) {
+								//console.log('finished all animations - auto-stopping');
+								self.stop();
+							}
 						}
 					}
 				}
